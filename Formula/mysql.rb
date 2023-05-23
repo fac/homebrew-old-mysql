95e827311ffe185dc6159e364e6950dac5549dba
Daniel George Holz <dgholz@gmail.com>
2023-05-23 11:30:56 +0100

    Rename file+class to pass tap audit

--- Formula/mysql.rb
+++ Formula/mysql.rb
@@ -1,4 +1,4 @@
-class MysqlAT8032 < Formula
+class Mysql < Formula
   desc "Open source relational database management system"
   homepage "https://dev.mysql.com/doc/refman/8.0/en/"
   url "https://cdn.mysql.com/Downloads/MySQL-8.0/mysql-boost-8.0.32.tar.gz"
